fab49f5082db1ce2d300c6d23a299496533cea0e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-12-08 21:42:40 -0300

    Se agrega SQL para tabla cliente en PostgreSQL

---
+++ Module/Sistema/Module/Empresa/Model/Sql/PostgreSQL/cliente.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+DROP TABLE IF EXISTS cliente CASCADE;
+CREATE TABLE cliente (
+	rut INTEGER PRIMARY KEY,
+	dv CHAR (1) NOT NULL,
+	razon_social CHARACTER VARYING (60) NOT NULL,
+	actividad_economica INTEGER,
+	email CHARACTER VARYING (50),
+	telefono CHARACTER VARYING (20),
+	direccion CHARACTER VARYING (100),
+	comuna CHAR (5),
+	CONSTRAINT cliente_actividad_economica_fk
+		FOREIGN KEY (actividad_economica)
+		REFERENCES actividad_economica (codigo) MATCH FULL
+		ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT cliente_comuna_fk
+		FOREIGN KEY (comuna)
+		REFERENCES comuna (codigo) MATCH FULL
+		ON UPDATE CASCADE ON DELETE CASCADE
+);
+COMMENT ON TABLE cliente IS 'Listado de clientes de la empresa';
+COMMENT ON COLUMN cliente.rut IS
+	'RUT del cliente, sin puntos ni dígito verificador';
+COMMENT ON COLUMN cliente.dv IS 'Dígito verificador del rut';
+COMMENT ON COLUMN cliente.razon_social IS 'Nombre o razón social';
+COMMENT ON COLUMN cliente.actividad_economica IS
+	'Actividad económica del cliente (si posee una)';
+COMMENT ON COLUMN cliente.email IS 'Correo electrónico principal de contacto';
+COMMENT ON COLUMN cliente.telefono IS 'Teléfono principal de contacto';
+COMMENT ON COLUMN cliente.direccion IS 'Dirección principal';
+COMMENT ON COLUMN cliente.comuna IS 'Comuna de la dirección';
+
+COMMIT;
