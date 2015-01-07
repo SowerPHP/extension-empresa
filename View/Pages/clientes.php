a0d5cd3c55cbe3eb61965f687a2fda43fefc7073
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-01-06 21:44:57 -0300

    Se utiliza Bootstrap por defecto para mostrar a los clientes, de todas formas se permite usar los boxAnimated si se define la constante CLIENTES_BOX_ANIMATED

--- View/Pages/clientes.php
+++ View/Pages/clientes.php
@@ -2,9 +2,39 @@
 <?php
 if (file_exists(DIR_PROJECT.'/data/clientes.php')) {
     include DIR_PROJECT.'/data/clientes.php';
-    \sowerphp\general\View_Helper_HTML::boxAnimated (
-        $clientes, $_base.'/img/pages/clientes', 'Solución entregada:'
-    );
+    if (defined('CLIENTES_BOX_ANIMATED') and CLIENTES_BOX_ANIMATED) {
+        \sowerphp\general\View_Helper_HTML::boxAnimated (
+            $clientes, $_base.'/img/pages/clientes', 'Solución entregada:'
+        );
+    } else {
+        if (!defined('CLIENTES_COLUMNAS'))
+            define('CLIENTES_COLUMNAS', 4);
+        $grid_cols = 12 / CLIENTES_COLUMNAS;
+        $i = 0;
+        echo '<div class="row">';
+        foreach ($clientes as $cliente => &$info) {
+            if ($i++%CLIENTES_COLUMNAS==0) {
+                echo '</div><div class="row">';
+            }
+            echo '<div class="col-md-',$grid_cols,'">';
+            echo '<div class="thumbnail">';
+            echo '<img src="',$_base.'/img/pages/clientes/',$info['imag'],'" alt="Logo ',$cliente,'">';
+            echo '<div class="caption">';
+            echo '<p>',$info['desc'],'</p>';
+            echo '<p><a href="#" tabindex="0" class="btn btn-primary" role="button" data-toggle="popover" data-trigger="focus" title="',$cliente,'" data-placement="top" data-content="',implode('. ', $info['info']),'" onclick="return false" onmouseover="$(this).popover(\'show\')" onmouseout="$(this).popover(\'hide\')">servicios prestados &raquo;</a></p>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+        }
+        echo '</div>',"\n";
+?>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+});
+</script>
+<?php
+    }
 } else {
     echo '<p>No existen datos de clientes disponibles.</p>';
 }
